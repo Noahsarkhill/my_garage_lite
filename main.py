61da19a09afa85e18a6832a888e7f33105aec3a9
Pollito <noahsarkhill@gmail.com>
2026-04-15 15:44:47 -0600

    Garage app v1.1.0 - add delete feature, input validation, and JSON save/load

--- main.py
+++ main.py
@@ -1,16 +1,89 @@
+import json
 from vehicle import Vehicle
 
 
-garage = []
+
+def save_garage(garage):
+    garage_data = []
+
+    for car in garage:
+        vehicle_dict = {
+            "year": car.year,
+            "make": car.make,
+            "model": car.model
+        }
+
+        garage_data.append(vehicle_dict)
+    
+    with open("garage.json", "w") as file:
+        json.dump(garage_data, file, indent=4)
+
+def load_garage():
+    try:
+        with open("garage.json", "r") as file:
+            garage_data = json.load(file)
+
+        loaded_garage = []
+
+        for car_data in garage_data:
+            vehicle = Vehicle(
+                car_data["year"],
+                car_data["make"],
+                car_data["model"]
+            )
+            loaded_garage.append(vehicle)
+
+        return loaded_garage
+    
+    except FileNotFoundError:
+        return []
+
+garage = load_garage()
+
 
 
 def add_vehicle(garage):
-    year = int(input("What is the year of your vehicle: "))
-    make = input("What is the make of your vehicle: ").title()
-    model = input("What is the model of your vehicle: ").title()
+    while True:
+        try:
+            year = int(input("What is the Year of your vehicle: "))
+            
+
+            if year < 1950 or year >= 2100:
+                print("Please enter a valid year")
+                continue
+
+            break
+
+        except ValueError:
+            print("Please enter a valid vear")
+
+    make = input("What is the Make of your vehicle: ")
+    model = input("What is the Model of your vehicle: ")
     new_vehicle = Vehicle(year, make, model)
 
     garage.append(new_vehicle)
+
+
+def delete_vehicle(garage):
+    if not garage:
+        print("Your Garage is empty")
+        return
+    while True:
+        try:
+            user_delete = int(input("Which car would you like to delete: ")) - 1
+
+            if user_delete < 0 or user_delete >= len(garage):
+                print("Invalid Selection")
+                continue
+    
+            deleted_car = garage.pop(user_delete)
+            print(f"You Deleted {deleted_car}")
+            break
+
+        except ValueError:
+            print("Please enter a number")
+
+    
 
 
 def view_garage(garage):
@@ -18,18 +91,21 @@ def view_garage(garage):
     while garage_open:
         print("GARAGE MENU")
         if not garage:
-            print("Your garage is empty")
+            print("Your Garage is empty")
         else:
             print("Your cars:")
-            for car in garage:
-                print(car)
+            for index, car in enumerate(garage):
+                print(index + 1, car)
         
-        print("1: Add Vehicle: ")
-        print("2: Exit Garage: ")
+        print("1: Add Vehicle")
+        print("2: Delete Vehicle")
+        print("3: Exit Garage")
         user_g_choice = input("what would you like to do: ")
         if user_g_choice == "1":
             add_vehicle(garage)
         elif user_g_choice == "2":
+            delete_vehicle(garage)
+        elif user_g_choice == "3":
             garage_open = False
 
 
@@ -53,6 +129,7 @@ def main_menu():
         elif user_choice == "2":
             view_garage(garage)
         elif user_choice == "3":
+            save_garage(garage)
             print("Good Bye")
             app_running = False
         else:
--- vehicle.py
+++ vehicle.py
@@ -1,7 +1,7 @@
 class Vehicle:
     def __init__(self, year, make, model):
         self.year = int(year)
-        self.make = make
-        self.model = model
+        self.make = make.title()
+        self.model = model.title()
     def __str__(self):
         return f"{self.year} {self.make} {self.model}"
