8af7eeb7f6ece525baffc574fd0ac97dd10fd61e
Pollito <noahsarkhill@gmail.com>
2026-04-20 03:33:14 -0600

    Garage app v1.2.0 ad search vehicles and helpers for validation and mileage for vehicles

--- vehicle.py
+++ vehicle.py
@@ -1,7 +1,8 @@
 class Vehicle:
-    def __init__(self, year, make, model):
+    def __init__(self, year, make, model, mileage):
         self.year = int(year)
         self.make = make.title()
         self.model = model.title()
+        self.mileage = int(mileage)
     def __str__(self):
-        return f"{self.year} {self.make} {self.model}"
+        return f"{self.year} {self.make} {self.model} - {self.mileage:,} miles"
